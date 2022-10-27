ae20ef6d068d69f0776b6c09fd191969ea65a075
stormBMO <andrey.chet76@gmail.com>
2022-10-27 22:06:09 +0300

    feat: clean project

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './search.css';
-import './main.css';
 // import App from './App';
 
 // ReactDOM.render(
